981e94b9180c2ec203ac5dd734745d301128b2a0
Cassiano Augusto <135405448+C4bs@users.noreply.github.com>
2024-09-19 22:49:19 -0300

    Update ContaTerminal.java

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        String conta = scanner.nextLine();
+
+        System.out.print("Por favor, digite o nome do cliente: ");
+        String cliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Agência: " + agencia);
+        System.out.println("Número da Conta: " + conta);
+        System.out.println("Cliente: " + cliente);
+        System.out.println("Saldo: " + saldo);
+
+        scanner.close();
     }
 }
