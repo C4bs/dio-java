ae766fa72c6b304818396b84b6260e4b106370c2
Cassiano Augusto <135405448+C4bs@users.noreply.github.com>
2024-10-07 20:28:21 -0300

    Create VericacaoComboCompleto.java

---
+++ VericacaoComboCompleto.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String verificarComboCompleto(String[] servicosContratados) {
+        
+        boolean movelContratado = false;
+        boolean bandaLargaContratada = false;
+        boolean tvContratada = false;
+
+        for (String servico : servicosContratados) {
+            switch (servico.toLowerCase()) {
+                case "movel":
+                    movelContratado = true;
+                    break;
+                case "banda larga":
+                    bandaLargaContratada = true;
+                    break;
+                case "tv":
+                    tvContratada = true;
+                    break;
+            }
+        }
+
+        if (movelContratado && bandaLargaContratada && tvContratada) {
+          return "Combo Completo";
+        } else {
+          return "Combo Incompleto";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" ");
+        String input = scanner.nextLine();
+        String[] servicosContratados = input.split(",");
+
+        String resultado = verificarComboCompleto(servicosContratados);
+        System.out.println(resultado);
+
+        scanner.close();
+    }
+}
